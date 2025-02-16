4b9c2b6e6ddbe501c23a1b16af6e3d93e08b10ec
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-02-17 01:28:13 +0600

    okay to?

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 // Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string. 
 
 function countWords(sentence){
-    let sentenceSplit = sentence.split(" ")
-    let count = sentenceSplit.length;
-
+    let sentenceSplit = sentence.split(" ");
+    let count=0;
+    for(let i of sentenceSplit){
+        count=count+1;
+    }
     return count;
 }
 
 
-const countWord = "DEV ZeroOne is a Next-Gen Programming Learning Platform."
+const countWord = "DEV ZeroOne is a Next-Gen Programming Learning Platform.";
 
 console.log(countWords(countWord));
