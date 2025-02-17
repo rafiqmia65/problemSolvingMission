19b99cfb348747688892597184949db2c8069f6a
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-02-17 10:31:51 +0600

    final push

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string. 
 
 function countWords(sentence){
+
+    if(typeof sentence !== "string"){
+        return "words number 0";
+    }
+
     let sentenceSplit = sentence.split(" ");
     let count=0;
     for(let i of sentenceSplit){
