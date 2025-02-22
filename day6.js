b7aa03e2bae796f9e623123ade19b009fa75d26f
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-02-22 18:15:35 +0600

    Upload day 6 problem

---
+++ day6.js
@@ -0,0 +1,26 @@
+// Write a JavaScript function filterIntegers  that takes an array that contains string and integer elements, and returns a new array with only integer elements.
+
+// Constraints:
+// Array length cannot be zero or negative.
+
+
+function filterIntegers( arr ){
+
+    if(arr.length <= 0){
+        return "Invalid Item"
+    }
+
+    let filterInteger =[];
+
+    for(let i of arr){
+        if(typeof i === "number"){
+            filterInteger.push(i);
+        }
+    }
+    return filterInteger;
+}
+
+
+// Example 1:
+console.log(filterIntegers([ 3, 6, "hello", 4, "3",  8]));
+// Output: [ 3, 6,  4,  8]`
