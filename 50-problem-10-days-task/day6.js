78b00b261c21460e817a80526823fd8b7ff4c12f
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-06-13 17:10:37 +0600

    solve: complete ES6+ Features problem 27

--- 50-problem-10-days-task/day6.js
+++ 50-problem-10-days-task/day6.js
@@ -13,3 +13,24 @@ const { firstName: name, lastName, age } = user;
 // console.log(name); // Output: 'Sara'
 // console.log(lastName); // Output: 'Khan'
 // console.log(age); // Output: 25
+
+//* Problem 27: Merge Arrays with Spread  [Easy]
+// Description: Write a function mergeArrays(...arrays) that accepts any number of arrays and merges them into one using the spread operator.
+// Example:
+// Input: [1,2], [3,4], [5]Output: [1,2,3,4,5]
+// Hint: Use rest parameters and spread inside reduce or flat.
+
+function mergeArrays(...arrays) {
+  //   return arrays.reduce((merged, arr) => [...merged, ...arr], []);
+
+  //  Alternatively, you can use flat:
+  return arrays.flat();
+
+  //   Alternatively, you can also use concat:
+  //   return [].concat(...arrays);
+}
+
+//* Test cases
+// console.log(mergeArrays([1, 2], [3, 4], [5])); // Output: [1, 2, 3, 4, 5]
+// console.log(mergeArrays(["a", "b"], ["c"], ["d", "e"])); // Output: ['a', 'b', 'c', 'd', 'e']
+
