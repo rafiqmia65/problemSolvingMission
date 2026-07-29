607339bcb736b4b64853c1125d5ef359f2a32305
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-07-29 14:49:57 +0600

    feat: solve Mission Twenty Problems #3

---
+++ Revision/MissionTwentyProblems/problem3.js
@@ -0,0 +1,45 @@
+// Write a JavaScript function `removeWords`  that takes a string as input and returns the remaining string after removing the first three characters.
+
+//1) If the input is null, undefined, or the empty string, it returns "Invalid input".
+//2) If the string length is three or less, it returns "String too short to remove".
+//3) Do not use any **built-in string methods** such as .slice() or .substring().
+
+// Input: "Dev Zero One"
+// Output: " Zero One"
+
+// Input: "I love programming"
+// Output: "ove programming"
+
+// Input: "Hello World"
+// Output: "lo World"
+
+// Input: "JS Rocks!"
+// Output: "Rocks!"
+
+// Input: "Yes"
+// Output: "String is too short to remove"
+
+// Input: ""
+// Output: "Input is Invalid"
+
+function removeWords(sentence) {
+  if (typeof sentence !== "string" || sentence.length === 0) {
+    return "Invalid input";
+  } else if (sentence.length <= 3) {
+    return "String too short to remove";
+  }
+
+  let remainingString = "";
+
+  for (let i = 3; i < sentence.length; i++) {
+    remainingString = remainingString + sentence[i];
+  }
+  return remainingString;
+}
+
+console.log(removeWords("Dev Zero One")); // Output: " Zero One"
+console.log(removeWords("I love programming")); // Output: "ove programming"
+console.log(removeWords("Hello World")); // Output: "lo World"
+console.log(removeWords("JS Rocks!")); // Output: "Rocks!"
+console.log(removeWords("Yes")); // Output: "String is too short to remove"
+console.log(removeWords("")); // Output: "Input is Invalid"
