6d97ac1f96199d4f0188fafa962c1b80afe29ee7
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-08-05 16:09:19 +0600

    feat: solve Mission Twenty Problems #5

---
+++ Revision/MissionTwentyProblems/problem5.js
@@ -0,0 +1,35 @@
+// Write a JS function `reverseArray` that takes an array and returns a reverse array.
+
+// **Constraints:**
+// 1. The function cannot change the main array.
+// 1. Cannot use array methods for reverse.
+
+function reverseArray(arr) {
+  if (!Array.isArray(arr)) {
+    return "please input a valid array";
+  }
+
+  let rev_arr = [];
+
+  for (let i of arr) {
+    rev_arr.unshift(i);
+  }
+  return rev_arr;
+}
+
+//* Another Way
+
+// function reverseArray(arr) {
+//   let rev_arr = [];
+
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     rev_arr.push(arr[i]);
+//   }
+
+//   return rev_arr;
+// }
+
+const numbers = [3, 6, 5, 4, 8];
+
+console.log("Original array:", numbers); // Output: [3, 6, 5, 4, 8]
+console.log("Reversed array:", reverseArray(numbers)); // Output: [8, 4, 5, 6, 3]
